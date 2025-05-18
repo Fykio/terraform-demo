2d3606b1f812e533706d6f9835562ef83436f583
Fykio <olufemi.akinboye@darey.io>
2025-05-18 20:36:09 +0100

    used s3 and ec2 demo

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "ubuntu_server" {
-    ami             = "ami-0cb91c7de36eed2cb"
-    instance_type   = "t2.micro"
+    ami             = "ami-0c1ac8a41498c1a9c"
+    instance_type   = "t3.micro"
     count           = 1
 }
 
-resource "aws_instance" "windows_server" {
-    ami             = "ami-0d0f28110d16ee7d6"
-    instance_type   = "t2.micro"
-    count           = 3
-}
+# resource "aws_instance" "windows_server" {
+#     ami             = "ami-0d0f28110d16ee7d6"
+#     instance_type   = "t2.micro"
+#     count           = 3
+# }
--- s3.tf
+++ s3.tf
@@ -1,15 +1,16 @@
-resource "aws_s3_bucket" "s3_bucket_special_1" {
-    bucket          = "our-s3-bucket-special-001"
+resource "aws_s3_bucket" "class_nova_s3_bucket" {
+    bucket          = "class-nova-s3-bucket-new-001"
+    force_destroy = true
 
     tags            = {
-        Name        = "our-s3-bucket-tag"
+        Name        = "class-nova"
     }
 }
 
-resource "aws_s3_bucket" "s3_bucket_special_2" {
-    bucket          = "our-s3-bucket-special-002"
+# resource "aws_s3_bucket" "s3_bucket_special_2" {
+#     bucket          = "our-s3-bucket-special-002"
 
-    tags            = {
-        Name        = "our-s3-bucket-tag"
-    }
-}
+#     tags            = {
+#         Name        = "our-s3-bucket-tag"
+#     }
+# }
